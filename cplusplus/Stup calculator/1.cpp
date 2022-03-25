ab5c7dbe912c5357e417ab124015f339a9028948
Vladislav <101063483+LepinVlados@users.noreply.github.com>
2022-03-25 14:39:56 +0300

    Salam

---
+++ cplusplus/Stup calculator/1.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+int add(int x, int y, int z) 
+{
+	std::cout << "Калькулятор очень глупый и высчитывает..\n";
+	return x + y + z; 
+}
+
+int main()
+{
+	std::cout << "Сумма 100 + 300 + 500 ? " << "\n";
+	std::cout << "Сумма будет равна = " << add(100, 300, 500) << "\n";
+	return 0;
+}
